8dc42eb3530e7f17331f5148ab6623f7ae37bde4
Gabriel Nascimento <opitimalracer50@hotmail.com>
2024-10-01 21:50:08 -0300

    Quest4

---
+++ quest4/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Dado os três lados de um triângulo determinar o perímetro do mesmo */
+Console.WriteLine("Insira o tamanho dos 3 lados do Triangulo para descobrir seu Perimetro");
+double L1, L2, L3, Perimetro;
+
+L1 = Convert.ToDouble(Console.ReadLine());
+L2 = Convert.ToDouble(Console.ReadLine());
+L3 = Convert.ToDouble(Console.ReadLine());
+
+Perimetro = L1 + L2 + L3;
+
+Console.WriteLine("Perimetro: "+Perimetro);
